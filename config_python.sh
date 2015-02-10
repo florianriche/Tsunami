5402b7012dab5a9722c461ce6413f0feafee2f56
Pierre <pierre@dhcp26-120.enst.fr>
2015-02-10 09:48:20 +0100

    config python

---
+++ config_python.sh
@@ -0,0 +1,15 @@
+sudo apt-get update
+sudo apt-get install build-essential gfortran gcc g++
+sudo apt-get install ipython
+sudo apt-get install python-pandas
+
+
+sudo apt-get install python-pip python-dev build-essential
+sudo pip install --upgrade pip
+sudo pip install --upgrade virtualenv
+
+pip install --upgrade setuptools
+
+pip install cassandra-driver
+pip install datetime
+
